9536592fa1578ea40c307728e77629945ebda3a7
Kallol Chakraborty <kallolchakraborty@hotmail.com>
2023-11-09 22:31:55 +0530

    Initial Commit

---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'dependencies' => ['backend'],
+    'tags' => [
+        'backend.form',
+    ],
+    'imports' => [
+        '@creativekallol/ckeditor-abbr/abbreviation.js' => 'EXT:ckeditor_abbr/Resources/Public/JavaScript/Plugin/abbreviation.js',
+    ],
+];
